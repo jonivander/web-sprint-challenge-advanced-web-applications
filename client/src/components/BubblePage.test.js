8b62e00b384835b191b18f7df74fb780459a4a31
jonathan warner <j.m.warners@gmail.com>
2020-08-21 12:05:00 -0700

    testing done. MVP complete

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -2,6 +2,40 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
+const colorData = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff"
+    },
+    id: 1
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc"
+    },
+    id: 2
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff"
+    },
+    id: 3
+  }
+]; 
+
+
+test("Fetches data and renders the bubbles", async() => {
   // Finish this test
+  const { rerender } = render(<BubblePage colors={[]}/>) 
+
+  let colorsArr = screen.queryAllByTestId(/colors/i);
+  expect(colorsArr).toHaveLength(0);
+
+  rerender(<BubblePage colors={colorData}/>); 
+
+  colorsArr = screen.queryAllByTestId(/colors/i);
+  expect(colorsArr).toHaveLength(3);
 });
