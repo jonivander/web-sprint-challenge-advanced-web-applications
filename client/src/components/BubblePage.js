7b7f487af25f8a8a5fde0f4839543250724a689a
jonathan warner <j.m.warners@gmail.com>
2020-08-21 10:54:04 -0700

    Got Login up and Colors to display

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+import { axiosWithAuth } from '../utils/axiosWithAuth'; 
+import { useParams } from 'react-router-dom'; 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const params = useParams(); 
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  
+
+    useEffect(() => {
+      axiosWithAuth()
+        .get(`/api/colors/`)
+        .then((res) => setColorList(res.data))
+        .catch((err) => console.log(err)); 
+    }, [params.id]); 
+  
+  
+ 
 
   return (
-    <>
+    <> 
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,68 @@
 import React from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth'; 
 
-const Login = () => {
+
+class Login extends React.Component {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+  state = {
+    credentials: {
+      username: '',
+      password: '',
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials, 
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/api/login', this.state.credentials)
+      .then((res) => {
+        // console.log(res.data.payload);
+        localStorage.setItem('token', res.data.payload); 
+        this.props.history.push('/bubblepage'); 
+      })
+      .catch((err) => console.log(err)); 
+  }
+
+  render() {
+      return (
+        <>
+        <form onSubmit={this.login}>
+          <h2>Login</h2>
+
+          <label>Username:
+            <input 
+              type='text'
+              name='username'
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            /> 
+          </label>
+
+          <label>Password:
+            <input 
+              type='text'
+              name='password'
+              value={this.state.credentials.password}
+              onChange={this.handleChange}
+            /> 
+          </label>
+
+          <button>Log in</button>
+
+        </form>
+        </>
+      );
+    };
+}
 
 export default Login;
